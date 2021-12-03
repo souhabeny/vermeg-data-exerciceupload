2ba092a6719343f13918696c3b4032aab3de51d4
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-12-03 14:36:41 +0100

    ListProvider OK

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -25,11 +25,62 @@ public class ProviderController {
 	@GetMapping("list")
 	//@ResponseBody
 	public String listProviders(Model model) {
-	model.addAttribute("providers", providerRepository.findAll());
-	System.out.println(providerRepository.findAll());
+		
+	List<Provider> lp = (List<Provider>)providerRepository.findAll();
+	if(lp.size() == 0)
+		lp = null;
+	model.addAttribute("providers", lp);
 	return "provider/listProviders";
-	//List<Provider> lp = (List<Provider>)providerRepository.findAll();
-	//System.out.println(lp);
-	//return "Nombre de fournisseur = " + lp.size();
+
 	}
+	
+	/*
+	 * 
+	 * @GetMapping("add")
+public String showAddProviderForm(Model model) {
+Provider provider = new Provider();// object dont la valeur des
+attributs par defaut
+model.addAttribute("provider", provider);
+return "provider/addProvider";
+}
+@PostMapping("add")
+public String addProvider(@Valid Provider provider,
+BindingResult result, Model model) {
+if (result.hasErrors()) {
+return "provider/addProvider";
+}
+providerRepository.save(provider);
+return "redirect:list";
+}
+@GetMapping("delete/{id}")
+public String deleteProvider(@PathVariable("id") long id, Model
+model) {
+//long id2 = 100L;
+Provider provider = providerRepository.findById(id)
+.orElseThrow(()-> new IllegalArgumentException("Invalid
+provider Id:" + id));
+System.out.println("suite du programme...");
+providerRepository.delete(provider);
+/*model.addAttribute("providers",
+providerRepository.findAll());
+return "provider/listProviders";*/
+/*return "redirect:../list";
+}
+@GetMapping("edit/{id}")
+public String showProviderFormToUpdate(@PathVariable("id") long
+id, Model model) {
+Provider provider = providerRepository.findById(id)
+.orElseThrow(()->new IllegalArgumentException("Invalid
+provider Id:" + id));
+model.addAttribute("provider", provider);
+return "provider/updateProvider";
+}
+@PostMapping("update")
+public String updateProvider(@Valid Provider provider,
+BindingResult result, Model model) {
+providerRepository.save(provider);
+return"redirect:list";
+}
+}
+	 */
 }
